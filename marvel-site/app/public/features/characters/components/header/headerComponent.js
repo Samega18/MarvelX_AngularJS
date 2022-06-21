951278d91259580141c962f84e61c0fb1169ec9c
Samuel Marques <69467232+Samega18@users.noreply.github.com>
2022-06-20 23:19:19 -0300

    First Commit

---
+++ marvel-site/app/public/features/characters/components/header/headerComponent.js
@@ -0,0 +1,5 @@
+angular.module("app").component("appHeader", {
+    templateUrl: "public/features/characters/components/header/header.html",
+    controller: function () {},
+    controllerAs: "ctrl",
+});
