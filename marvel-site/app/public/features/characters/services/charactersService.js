aaeceb4d09c5584649ee8c2dd19bbcda965eb5e9
Samuel Marques <69467232+Samega18@users.noreply.github.com>
2022-06-29 18:44:12 -0300

    Update

--- marvel-site/app/public/features/characters/services/charactersService.js
+++ marvel-site/app/public/features/characters/services/charactersService.js
@@ -1,8 +1,8 @@
 angular.module("app").service("CharactersService", function ($http, env) {
   this.getAllCharacters = (name, offset, limit, categorie) => {
     const params = {
-      apikey: "00c502c2b771a984e4cb31fea3921b14",
-      hash: "6a16c993302911bceb2491e49926426b",
+      apikey: "f79245e035e64fb032fd90e1e446d36f",
+      hash: "03ef7068f9411d1f8fcd969b4e0ee2e2",
       ts: 1,
       offset,
       limit,
@@ -26,8 +26,8 @@ angular.module("app").service("CharactersService", function ($http, env) {
   this.getDetails = (categorie, id) => {
 
     const params = {
-      apikey: '00c502c2b771a984e4cb31fea3921b14',
-      hash: '6a16c993302911bceb2491e49926426b',
+      apikey: "f79245e035e64fb032fd90e1e446d36f",
+      hash: "03ef7068f9411d1f8fcd969b4e0ee2e2",
       ts: 1,
     };
 
@@ -39,8 +39,8 @@ angular.module("app").service("CharactersService", function ($http, env) {
   this.getDetailView = (categorie, id, categorieView, limit, offset) => {
 
     const params = {
-      apikey: '00c502c2b771a984e4cb31fea3921b14',
-      hash: '6a16c993302911bceb2491e49926426b',
+      apikey: "f79245e035e64fb032fd90e1e446d36f",
+      hash: "03ef7068f9411d1f8fcd969b4e0ee2e2",
       ts: 1,
       offset,
       limit,
@@ -51,3 +51,15 @@ angular.module("app").service("CharactersService", function ($http, env) {
     });
   };
 });
+
+
+
+/* Dados anteriores
+
+const params = {
+  apikey: "00c502c2b771a984e4cb31fea3921b14",
+  hash: "6a16c993302911bceb2491e49926426b",
+  ts: 1,
+  offset,
+  limit,
+}; */
